7a921f43d084473a4f0cd1041d4f64338355714d
Vivek504 <viveksonani302@gmail.com>
2022-01-20 16:26:18 +0530

    mic completed

--- online-meet/public/script.js
+++ online-meet/public/script.js
@@ -5,23 +5,24 @@ const myPeer = new Peer(undefined, {
     port: '3001'
 })
 const myVideo = document.createElement('video')
-myVideo.muted = true
+let myVideoStream;
 
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
+    myVideoStream = stream
     addVideoStream(myVideo, stream)
     myPeer.on('call', call => {
         call.answer(stream)
         const video = document.createElement('video')
         call.on('stream', userVideoStream => {
-          addVideoStream(video, userVideoStream)
+            addVideoStream(video, userVideoStream)
         })
-      })
-      socket.on('user-connected', userId => {
+    })
+    socket.on('user-connected', userId => {
         // connectToNewUser(userId, stream)
-        setTimeout(connectToNewUser,3000,userId,stream);
+        setTimeout(connectToNewUser, 3000, userId, stream);
     })
 })
 
@@ -36,15 +37,73 @@ function connectToNewUser(userId, stream) {
         addVideoStream(video, userVideoStream)
     })
     call.on('close', () => {
-      video.remove()
+        video.remove()
     })
-  
-    // peers[userId] = call
 }
 function addVideoStream(video, stream) {
-    video.srcObject = stream
+    stream.getVideoTracks()[0].enabled = false;
+    stream.getAudioTracks()[0].enabled = false;
+    video.srcObject = myVideoStream
     video.addEventListener('loadedmetadata', () => {
         video.play()
     })
-    videoGrid.append(video)
+    videoGrid.append(video);
+}
+
+const micOnOff = () => {
+    let enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        turnOffMic();
+    } else {
+        turnOnMic();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const turnOffMic = () => {
+    const html = `
+    <span class="material-icons">
+        mic_off
+      </span>
+    `
+    document.querySelector('.mic-icon').innerHTML = html;
+}
+
+const turnOnMic = () => {
+    const html = `
+    <span class="material-icons">
+        mic
+      </span>
+    `
+    document.querySelector('.mic-icon').innerHTML = html;
+}
+
+const videoOnOff = () => {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        turnOffVideo();
+    } else {
+        turnOnVideo();
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+const turnOffVideo = () => {
+    const html = `
+    <span class="material-icons">
+        videocam_off
+      </span>
+    `
+    document.querySelector('.video-icon').innerHTML = html;
+}
+
+const turnOnVideo = () => {
+    const html = `
+    <span class="material-icons">
+        videocam
+      </span>
+    `
+    document.querySelector('.video-icon').innerHTML = html;
 }
